6160d721329d7a803e22ae338547202c835d7b2d
Koki Ibukuro <koki.ibukuro@gmail.com>
2015-01-15 20:07:36 +0900

    Add new sample.

---
+++ UnityKonashiSample/Assets/Konashi/Sample/Scripts/KonashiSample3D.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Konashi {
+	public class KonashiSample3D : MonoBehaviour {
+		
+		// set your konashi name
+		public string konashiName = "konashi#1-2345";
+		
+		void Start () {
+			
+			var konashi = KonashiPlugin.instance;
+			konashi.Initialize();
+			
+			konashi.OnReady += () => {
+				KonashiPlugin.PinMode(KonashiDigitalIOPin.DigitalIO1, KonashiPinMode.Output);
+				KonashiPlugin.PinMode(KonashiDigitalIOPin.DigitalIO2, KonashiPinMode.Output);
+				KonashiPlugin.PinMode(KonashiDigitalIOPin.DigitalIO3, KonashiPinMode.Output);
+			};
+			
+			KonashiPlugin.Find(konashiName);
+		}
+		
+		
+		
+		
+		
+	}
+}
---
+++ UnityKonashiSample/Assets/Konashi/Sample/Scripts/LedBall.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Konashi
+{
+	[RequireComponent(typeof(Collider))]
+	[RequireComponent(typeof(MeshRenderer))]
+	public class LedBall : MonoBehaviour
+	{
+		[RangeAttribute(0.0f, 1.0f)]
+		public float emission;
+		
+		public Color emissionColor0 = Color.black;
+		public Color emissionColor1 = Color.white;
+		
+		public KonashiDigitalIOPin pin;
+		
+		
+		Material mat;
+		int matID_EmissionColor;
+		bool touched;
+		
+		void OnEnable()
+		{
+			matID_EmissionColor = Shader.PropertyToID("_EmissionColor");
+			mat = this.GetComponent<MeshRenderer>().material;
+		}
+		
+		
+		void Update()
+		{
+			float target = touched ? 1.0f : 0.0f;
+			emission += (target - emission) * 0.1f;
+			
+			mat.SetColor(matID_EmissionColor, Color.Lerp(emissionColor0, emissionColor1, emission));
+		}
+		
+		
+		void OnMouseDown()
+		{
+			touched = true;
+			KonashiPlugin.DigitalWrite(pin, KonashiLevel.High);
+		}
+		
+		void OnMouseUp()
+		{
+			touched = false;
+			KonashiPlugin.DigitalWrite(pin, KonashiLevel.Low);
+		}
+	}
+	
+}
